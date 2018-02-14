f555a6c076bb740e89d14465b958292618b3deaf
arpban <arpit.bansal2010@gmail.com>
2018-02-14 21:28:10 +0530

    serverjs

--- out/_next/1f227e19-03c1-4e52-9ca9-b168d181d3de/page/2018.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/2018', function() {
-            var comp = module.exports=webpackJsonp([5],{459:function(e,t,u){e.exports=u(460)},460:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(49),o=function(e){return e&&e.__esModule?e:{default:e}}(n);t.default=(0,o.default)("/essays")},49:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var o=u(9),r=n(o),a=u(4),l=n(a),f=u(5),i=n(f),d=u(10),s=n(d),c=u(11),p=n(c),_=u(1),v=u(19),h=n(v),y=function(e){return function(t){function u(){return(0,l.default)(this,u),(0,s.default)(this,(u.__proto__||(0,r.default)(u)).apply(this,arguments))}return(0,p.default)(u,t),(0,i.default)(u,null,[{key:"getInitialProps",value:function(t){var u=t.res;return u?(u.writeHead(301,{Location:e}),u.end()):"/"===e.charAt(0)?h.default.push(e):window.location=e,{}}}]),u}(_.Component)};t.default=y}},[459]);
-            return { page: comp.default }
-          })
-        
