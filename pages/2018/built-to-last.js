86471305926f3e1111da396f6b9939cb8438cb7c
arpban <arpit.bansal2010@gmail.com>
2018-02-17 18:55:53 +0530

    built-to-last

---
+++ pages/2018/built-to-last.js
@@ -0,0 +1,41 @@
+// Syntax
+import js from 'highlight.js/lib/languages/javascript'
+
+// Layouts
+import Post from '../../layouts/essay'
+
+// Components
+import P from '../../components/paragraph'
+import Link from '../../components/link'
+import Meta from '../../components/meta'
+
+export default () => (
+  <Post>
+    <Meta category="/essays" id="built-to-last"/>
+
+<P>I just finished reading this book by Jim Collins and here's what i learned.</P>
+<P>
+In this book, author talks about the importance of being a clock builder than a time teller.
+What most visionary companies have in common is that their founders were more focused on building a great company. It does not matter that you have a great idea. what matters is that your company has a set of core values. Don't wait for the great idea, instead focus on building a great company with set of values which don't change over time. Focus on building a great organization, which wants to improve this world through its products. Most people think that they need a great idea to build a visionary company. But it is completely false. Most visionary companies started without any great idea.
+</P>
+<P>
+Visionary companies have a clear set of core values, which never change. their product may change overtime but the set of core values always remain same. 
+they set big goals.. which usually seem impossible and crazy to outside world. but these big and clear set of goals is what makes them achieve them. These big goals are very easy to explain, for example, john f kennedy said that by the end of the decade, humans will set foot on the moon. now this is a big and motivating goal that could push people to work. It's a goal that could inspire people to work.
+</P>
+<P>
+These visionary companies dont have much heirarchy. People are free to make mistakes as long as they understand their company's core values. These kind of companies evolve much like the darwin's theory of evolution. These companies follow the principle of trying quickly and rejecting the weak ideas quickly.
+</P>
+<P>
+These companies provide space to their employees- to experiment, come up with their own ideas, they kindle the spirit of entrepreneurship in the employees within the company. Companies like 3M allows employees to spend 15% of time to their own projects. These companies allow people to make mistakes which is what makes them to produce great ideas. 
+whereas in a typical dull company- an employee may be humiliated on failures and very less freedom is given to experiment. Remember when you put employees in a fence, they will become like sheep.
+</P>
+<P>
+Visionary companies have a slightly cult like cultures in them. They rigorously follow programs which teach employees the core values of the company.
+</P>
+<P>
+Remember never to humiliate any employee for his/her mistakes or ideas. Never reject ideas very quickly. Try and fail quick. Your employees should not be scared of you. It should not be that they are afraid of coming up with new ideas. This could happen if you humiliate them.
+</P>
+ 
+
+  </Post>
+)
